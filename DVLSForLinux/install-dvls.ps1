1b91f71155660b1294b240cd598d870e7a821d01
Benoît CORTIER <bcortier@proton.me>
2024-10-09 00:14:48 +0900

    style(DVLSForLinux): run formatter

--- DVLSForLinux/install-dvls.ps1
+++ DVLSForLinux/install-dvls.ps1
@@ -62,32 +62,36 @@ $DVLSVariables = @{
     'DVLSCertificate'                = $False
 }
 
-If (Test-Path -Path $DVLSVariables.SystemDPath) {
+if (Test-Path -Path $DVLSVariables.SystemDPath) {
     Write-Error ("[{0}] An existing Devolutions Server of Linux (Beta) SystemD Unit file already appears, aborting installation" -F (Get-Date -Format "yyyyMMddHHmmss"))
     Exit
 }
 
-If ($GenerateSelfSignedCertificate -Is [Bool]) {
+if ($GenerateSelfSignedCertificate -Is [Bool]) {
     $DVLSVariables.DVLSCertificate = [Bool]$GenerateSelfSignedCertificate
-} Else {
+}
+else {
     $DVLSVariables.DVLSCertificate = ($Host.UI.PromptForChoice("", "Generate self-signed certificate?", @('&Yes', '&No'), 1)) ? $False : $True
 }
 
-If ($DVLSVariables.DVLSCertificate) {
+if ($DVLSVariables.DVLSCertificate) {
     $DVLSVariables.DVLSURI = ("https://{0}:5000/" -F $DVLSVariables.DVLSHostName)
-} Else {
+}
+else {
     $DVLSVariables.DVLSURI = ("http://{0}:5000/" -F $DVLSVariables.DVLSHostName)
 }
 
-If ($DatabaseEncryptedConnection -Is [Bool]) {
+if ($DatabaseEncryptedConnection -Is [Bool]) {
     $DVLSVariables.UseEncryptedConnection = [Bool]$DatabaseEncryptedConnection
-} Else {
+}
+else {
     # TODO: prompt
 }
 
-If ($DatabaseTrustServerCertificate -Is [Bool]) {
+if ($DatabaseTrustServerCertificate -Is [Bool]) {
     $DVLSVariables.TrustServerCertificate = [Bool]$DatabaseTrustServerCertificate
-} Else {
+}
+else {
     # TODO: prompt
 }
 
@@ -107,9 +111,10 @@ $DVLSVariables | Select-Object -Property @(
     @{
         'Name'       = 'DatabaseEncryptedConnection'
         'Expression' = {
-            If ($PSItem.DatabaseEncryptedConnection -Eq $Null) {
+            if ($PSItem.DatabaseEncryptedConnection -Eq $Null) {
                 'Undefined'
-            } Else {
+            }
+            else {
                 $PSItem.DatabaseEncryptedConnection
             }
         }
@@ -117,23 +122,24 @@ $DVLSVariables | Select-Object -Property @(
     @{
         'Name'       = 'DatabaseTrustServerCertificate'
         'Expression' = {
-            If ($PSItem.DatabaseTrustServerCertificate -Eq $Null) {
+            if ($PSItem.DatabaseTrustServerCertificate -Eq $Null) {
                 'Undefined'
-            } Else {
+            }
+            else {
                 $PSItem.DatabaseTrustServerCertificate
             }
         }
     }
 ) | Format-List
 
-If ($Confirm) {
+if ($Confirm) {
     Write-Warning "If the above values look correct, enter [Y] or press Enter to continue" -WarningAction Inquire
 }
 
 Write-Verbose ("[{0}] Requesting 'sudo -v' for cached credentials" -F (Get-Date -Format "yyyyMMddHHmmss"))
 & sudo -v
 
-If (-Not [Bool](Get-Module -ListAvailable -Name 'Devolutions.PowerShell')) {
+if (-Not [Bool](Get-Module -ListAvailable -Name 'Devolutions.PowerShell')) {
     Write-Host ("[{0}] Installing Devolutions.PowerShell module for all users" -F (Get-Date -Format "yyyyMMddHHmmss")) -ForegroundColor Green
 
     & sudo $PwshExecutable -Command {
@@ -141,14 +147,15 @@ If (-Not [Bool](Get-Module -ListAvailable -Name 'Devolutions.PowerShell')) {
     }
 }
 
-Try {
+try {
     Import-Module -Name 'Devolutions.PowerShell' -Scope 'Global' -ErrorAction 'Stop'
-} Catch {
+}
+catch {
     Write-Error ("[{0}] The Devolutions.PowerShell module failed to load, aborting installation" -F (Get-Date -Format "yyyyMMddHHmmss"))
     Exit
 }
 
-If (-Not [Bool](Get-Module -Name 'Devolutions.PowerShell')) {
+if (-Not [Bool](Get-Module -Name 'Devolutions.PowerShell')) {
     Write-Error ("[{0}] The Devolutions.PowerShell module failed to install, aborting installation" -F (Get-Date -Format "yyyyMMddHHmmss"))
     Exit
 }
@@ -175,32 +182,32 @@ Write-Verbose ("[{0}] Switching group to '{1}'" -F (Get-Date -Format "yyyyMMddHH
 
 Write-Verbose ("[{0}] Validating users, groups, and directories" -F (Get-Date -Format "yyyyMMddHHmmss"))
 
-$validateDVLSUser  = & id -u $DVLSVariables.DVLSUser
+$validateDVLSUser = & id -u $DVLSVariables.DVLSUser
 $validateDVLSGroup = & getent group $DVLSVariables.DVLSGroup
 
-If (-Not $validateDVLSUser) {
+if (-Not $validateDVLSUser) {
     Write-Error ("[{0}] User, '{1}', is missing" -F (Get-Date -Format "yyyyMMddHHmmss"), $DVLSVariables.DVLSUser)
     Exit
 }
 
-If (-Not $validateDVLSGroup) {
+if (-Not $validateDVLSGroup) {
     Write-Error ("[{0}] Group, '{1}', is missing" -F (Get-Date -Format "yyyyMMddHHmmss"), $DVLSVariables.DVLSGroup)
     Exit
 }
 
 $validateDVLSGroupUsers = (($validateDVLSGroup -Split ":")[-1] -Split ",")
 
-If (-Not ($validateDVLSGroupUsers -Contains $DVLSVariables.DVLSUser -And $validateDVLSGroupUsers -Contains $DVLSVariables.CurrentUser)) {
+if (-Not ($validateDVLSGroupUsers -Contains $DVLSVariables.DVLSUser -And $validateDVLSGroupUsers -Contains $DVLSVariables.CurrentUser)) {
     Write-Error ("[{0}] User, '{1}' and '{2}', are not members of the group, '{3}'" -F (Get-Date -Format "yyyyMMddHHmmss"), $DVLSVariables.DVLSUser, $DVLSVariables.CurrentUser, $DVLSVariables.DVLSGroup)
     Exit
 }
 
-If (-Not ((& stat -c %a ([System.IO.DirectoryInfo]$DVLSVariables.DVLSPath).Parent) -EQ '755' -And (& stat -c %a $DVLSVariables.DVLSPath) -EQ '755')) {
+if (-Not ((& stat -c %a ([System.IO.DirectoryInfo]$DVLSVariables.DVLSPath).Parent) -EQ '755' -And (& stat -c %a $DVLSVariables.DVLSPath) -EQ '755')) {
     Write-Error ("[{0}] Permissions on '{1}' are incorrect" -F (Get-Date -Format "yyyyMMddHHmmss"), $DVLSVariables.DVLSPath)
     Exit
 }
 
-If ( -Not
+if ( -Not
     (
         ((Get-Item -Path ([System.IO.DirectoryInfo]$DVLSVariables.DVLSPath).Parent).User -EQ $DVLSVariables.DVLSUser) -And
         ((Get-Item -Path ([System.IO.DirectoryInfo]$DVLSVariables.DVLSPath).Parent).Group -EQ $DVLSVariables.DVLSGroup) -And
@@ -262,7 +269,7 @@ $AppSettingsExists = & sudo $PwshExecutable -Command {
     Test-Path -Path (Join-Path -Path $DVLSVariables.DVLSPath -ChildPath 'appsettings.json')
 } -Args $DVLSVariables
 
-If (-Not $AppSettingsExists) {
+if (-Not $AppSettingsExists) {
     Write-Error ("[{0}] appsettings.json does not exist or is inaccessible" -F (Get-Date -Format "yyyyMMddHHmmss"))
     Exit
 }
@@ -292,7 +299,7 @@ New-DPSAppsettings -Configuration $Configuration
 
 $Settings = Get-DPSAppSettings -ApplicationPath $DVLSVariables.DVLSPath
 
-If ($DVLSVariables.CreateDatabase) {
+if ($DVLSVariables.CreateDatabase) {
     New-DPSDatabase -ConnectionString $Settings.ConnectionStrings.LocalSqlServer
 }
 
@@ -303,20 +310,20 @@ New-DPSEncryptConfiguration -ApplicationPath $DVLSVariables.DVLSPath
 New-DPSDatabaseAppSettings -Configuration $Configuration
 New-DPSAdministrator -ConnectionString $Settings.ConnectionStrings.LocalSqlServer -Name $DVLSVariables.DVLSAdminUsername -Password $DVLSVariables.DVLSAdminPassword -Email $DVLSVariables.DVLSAdminEmail
 
-If ($DVLSVariables.DVLSCertificate) {
+if ($DVLSVariables.DVLSCertificate) {
     Write-Host ("[{0}] Generating self-signed certificate" -F (Get-Date -Format "yyyyMMddHHmmss")) -ForegroundColor Green
 
     & openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout cert.key -out cert.crt -subj ("/CN={0}" -F $DVLSVariables.DVLSHostName) -addext ("subjectAltName=IP:{0}" -F $DVLSVariables.DVLSHostName) > /dev/null 2>&1
     & openssl pkcs12 -export -out cert.pfx -inkey cert.key -in cert.crt -passout pass: > /dev/null 2>&1
 
-    $JSON = Get-Content -Path (Join-Path -Path $DVLSVariables.DVLSPath -ChildPath "appsettings.json") | ConvertFrom-JSON -Depth 100
+    $JSON = Get-Content -Path (Join-Path -Path $DVLSVariables.DVLSPath -ChildPath "appsettings.json") | ConvertFrom-Json -Depth 100
 
     $JSON.Kestrel.Endpoints.Http | Add-Member -MemberType NoteProperty -Name "Certificate" -Value @{
         "Path"     = (Join-Path -Path $DVLSVariables.DVLSPath -ChildPath "cert.pfx")
         "Password" = ""
     }
 
-    $JSON | ConvertTo-JSON -Depth 100 | Set-Content -Path (Join-Path -Path $DVLSVariables.DVLSPath -ChildPath "appsettings.json")
+    $JSON | ConvertTo-Json -Depth 100 | Set-Content -Path (Join-Path -Path $DVLSVariables.DVLSPath -ChildPath "appsettings.json")
 
     Set-DPSAccessUri -ApplicationPath $DVLSVariables.DVLSPath -ConnectionString $Settings.ConnectionStrings.LocalSqlServer -AccessURI ("https://{0}:5000/" -F $DVLSVariables.DVLSHostName)
 
@@ -350,7 +357,7 @@ Alias=dvls.service
 
 & sudo $PwshExecutable -Command { Param($DVLSVariables, $SystemDTemplate); Set-Content -Path $DVLSVariables.SystemDPath -Value $SystemDTemplate -Force; & systemctl daemon-reload } -Args $DVLSVariables, $SystemDTemplate
 
-If (-Not (Test-Path -Path $DVLSVariables.SystemDPath)) {
+if (-Not (Test-Path -Path $DVLSVariables.SystemDPath)) {
     Write-Error ("[{0}] systemd unit file missing at '{1}'" -F (Get-Date -Format "yyyyMMddHHmmss"), $DVLSVariables.SystemDPath)
     Exit
 }
@@ -367,14 +374,17 @@ Write-Host ("[{0}] Restart $DvlsForLinuxName at '{1}'" -F (Get-Date -Format "yyy
 
 $Result = & systemctl list-units --type=service --all --no-pager dvls.service --no-legend
 
-If ($Result) {
+if ($Result) {
     $ID, $Load, $Active, $Status, $Description = ($Result.Trim()) -Split '\s+', 5
 
-    If ($ID -And $Active -And ($Status -EQ 'running')) {
+    if ($ID -And $Active -And ($Status -EQ 'running')) {
         Write-Host ("[{0}] $DvlsForLinuxName is running" -F (Get-Date -Format "yyyyMMddHHmmss")) -ForegroundColor Green
-    } Else {
+    }
+    else {
         Write-Error ("[{0}] $DvlsForLinuxName service status was not found" -F (Get-Date -Format "yyyyMMddHHmmss"))
     }
-} Else {
+}
+else {
     Write-Error ("[{0}] $DvlsForLinuxName failed to start" -F (Get-Date -Format "yyyyMMddHHmmss"))
 }
+
